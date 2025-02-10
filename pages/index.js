644401844436c3143252ebe947df8f9a6a0cc78e
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-02-10 20:39:10 +0530

    ui components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+"use client";
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+import { useRouter } from "next/navigation"; // Import useRouter
+
 export default function HomePage() {
-    return (
-      <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-        <h1 className="text-3xl font-bold">Welcome to Jai Raj's Slam!</h1>
-        <p className="text-lg text-gray-600">Start by navigating to the login page.</p>
-      </div>
-    );
-  }
-  
+  const router = useRouter(); // Initialize the router
+
+  return (
+    <BackgroundBeamsWithCollision className="flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-bold text-white mb-[2rem]">
+        Welcome to Jai Raj's Slam!
+      </h1>
+      <p className="text-3xl text-gray-600">Start by navigating to the login page.</p>
+
+      <button
+        className="mt-[3rem] bg-white text-black font-semibold py-2 px-4 rounded-3xl text-xl transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-purple-500 hover:to-blue-500 hover:text-white"
+        onClick={() => router.push("/auth/login")} // Navigate to /login page
+      >
+        Get started
+      </button>
+    </BackgroundBeamsWithCollision>
+  );
+}
