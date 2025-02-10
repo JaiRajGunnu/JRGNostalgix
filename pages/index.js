afecee407c66dc08297e86b29536b313f8149193
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-02-10 22:21:30 +0530

    tit name

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function HomePage() {
     <>
       {/* ✅ Set Page Title & Favicon */}
       <Head>
-        <title>Home</title>
+        <title>Jai Raj's Slam Book</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
