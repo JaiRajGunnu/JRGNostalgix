0d1c89ba9540ea84796a603c234189b81165c5ff
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-02-11 17:10:57 +0530

    css

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -96,7 +96,7 @@ export const DesktopSidebar = ({
                     className
                 )}
                 animate={{
-                    width: animate ? (open ? "300px" : "65px") : "300px",
+                    width: animate ? (open ? "250px" : "65px") : "250px",
                 }}
                 onMouseEnter={() => setOpen(true)}
                 onMouseLeave={() => setOpen(false)}
