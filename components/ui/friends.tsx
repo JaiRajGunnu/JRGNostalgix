e7c736c07d9ffecf504d42a856c95a93d1c52f68
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-02-11 13:25:32 +0530

    img test path fix

--- components/ui/friends.tsx
+++ components/ui/friends.tsx
@@ -11,34 +11,34 @@ type Testimonial = {
   designation: string;
   src: string;
 };
-
 // Fake Testimonials Data
 export const fakeTestimonials: Testimonial[] = [
   {
     quote: "Junnu's coding skills are next level! He builds amazing projects with such ease and creativity.",
     name: "Kanna",
     designation: "Software Developer",
-    src: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    src: "/img/img1.avif", 
   },
   {
     quote: "Junnu is an amazing friend! Always supportive and kind. Blessed to have him in my life.",
     name: "Chinnuu",
     designation: "Best Friend Forever",
-    src: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    src: "/img/img2.avif", 
   },
   {
     quote: "Junnu's dedication and passion for tech are truly inspiring. He never stops learning!",
     name: "Vissu",
     designation: "Web Developer",
-    src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    src: "/img/img3.avif", 
   },
   {
     quote: "A true problem solver! Junnu has a unique way of tackling challenges and coming up with brilliant solutions.",
     name: "Likki",
     designation: "Cloud Engineer",
-    src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    src: "/img/img4.avif", 
   },
 ];
+
 
 
 export const Friends = ({
