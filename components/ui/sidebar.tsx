80569dc704d2c1f35e866e2be8c2f46c540c1570
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-02-17 18:55:22 +0530

    sidebar

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -128,7 +128,7 @@ export const MobileSidebar = ({
         <>
             <div
                 className={cn(
-                    "h-13 px-4 py-4 flex flex-row md:hidden items-center justify-between bg-neutral-100 dark:bg-[#141414] w-full"
+                    "h-12 px-4 py-4 flex flex-row md:hidden items-center justify-between bg-neutral-100 dark:bg-[#141414] w-full"
                 )}
                 {...props}
             >
