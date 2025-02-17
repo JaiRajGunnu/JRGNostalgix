77ae07e188a042a0351164042f8d59978c3be0f0
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-02-17 20:49:43 +0530

    mob resp 15

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -50,8 +50,8 @@ const FeedbackPage = () => {
 
       <SidebarLayout>
         <BackgroundBeamsWithCollision className="p-[5%]  flex flex-col justify-center items-center w-full">
-          <div className="flex flex-col items-center justify-center min-h-screen mt-[-10%] md:mt-0 lg:mt-0 ">
-            <h2 className="text-4xl font-bold mb-10 text-center text-gray-800 dark:text-white">
+          <div className="flex flex-col items-center justify-center min-h-screen mt-[-20%] md:mt-0 lg:mt-0 ">
+            <h2 className="text-4xl font-bold font-poppins  mb-10 text-center text-gray-800 dark:text-white">
               How was your experience?
             </h2>
             <div className="flex space-x-7">
