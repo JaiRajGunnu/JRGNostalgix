474662018ed42107c353751f123683f720178c87
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-02-19 10:29:25 +0530

    alrt mob resp 2

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -78,7 +78,7 @@ const FeedbackPage = () => {
 
           {/* ✅ Floating Message */}
           {feedbackSubmitted && (
-            <div className="fixed bottom-5 right-5 bg-[#262626] text-white px-5 py-3 rounded-lg shadow-lg opacity-100 transition-opacity animate-fadeIn">
+            <div className="fixed m-5 bottom-5 right-0 md:bottom-10 md:right-10 lg:bottom-10 lg:right-10 bg-[#262626] text-white px-5 py-3 rounded-lg shadow-lg opacity-100 transition-opacity animate-fadeIn">
               Thanks for your feedback. 😊
             </div>
           )}
