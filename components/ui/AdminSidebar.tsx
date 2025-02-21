a871cbe7befdc72a54e773be53da3a1a7463d445
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-02-21 23:34:27 +0530

    icons

--- components/ui/AdminSidebar.tsx
+++ components/ui/AdminSidebar.tsx
@@ -1,6 +1,9 @@
 import { FaUsers, FaComments, FaEye, FaLock, FaChevronDown } from "react-icons/fa";  
 import Link from "next/link";
 import { useState } from "react";
+import { MdOutlineDashboard } from "react-icons/md";
+import { TbUsersGroup } from "react-icons/tb";
+import { MdOutlineAdminPanelSettings } from "react-icons/md";
 
 const AdminSidebar = () => {
   const [isPagesOpen, setIsPagesOpen] = useState(true);
@@ -8,9 +11,9 @@ const AdminSidebar = () => {
   const [isUserPagesOpen, setIsUserPagesOpen] = useState(true);
 
   const links = [
-    { label: "Dashboard", href: "/admin", icon: <FaEye className="h-6 w-6" /> },
-    { label: "Users", href: "/admin/users", icon: <FaUsers className="h-6 w-6" /> },
-    { label: "Admins List", href: "/admin/adminslist", icon: <FaLock className="h-6 w-6" /> },
+    { label: "Dashboard", href: "/admin", icon: <MdOutlineDashboard className="h-6 w-6" /> },
+    { label: "Users", href: "/admin/users", icon: <TbUsersGroup      className="h-6 w-6" /> },
+    { label: "Admins List", href: "/admin/adminslist", icon: <MdOutlineAdminPanelSettings  className="h-6 w-6" /> },
   ];
 
   const pilotPages = [
@@ -29,7 +32,7 @@ const AdminSidebar = () => {
   ];
 
   return (
-    <aside className="fixed top-0 left-0 w-64 h-full bg-[#17181a] text-gray-200 p-6">
+    <aside className="fixed top-0 left-0 w-64 h-full bg-[#17181a] text-gray-200 p-6 font-poppins">
       <h2 className="text-2xl font-bold">Admin Panel</h2>
       <nav className="mt-6">
         <ul>
@@ -37,7 +40,7 @@ const AdminSidebar = () => {
             <li key={idx} className="mt-4 cursor-pointer hover:text-gray-400">
               <Link href={link.href} className="flex items-center">
                 {link.icon}
-                <span className="ml-2">{link.label}</span>
+                <span className="ml-3">{link.label}</span>
               </Link>
             </li>
           ))}
