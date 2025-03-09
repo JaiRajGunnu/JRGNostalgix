3c604a544d174212e4a80b7ce48a841e09b93a89
Jai Raj Gunnu <jairajgsklm@gmail.com>
2025-03-09 14:53:17 +0530

    build fix

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -224,7 +224,7 @@ export default function Login() {
 
             {/* Register Link */}
             <p className="text-gray-400 text-center text-sm scale-95 font-poppins">
-              Don't have an account?{" "}
+              Don&apos;t have an account?{" "}
               <Link href="/auth/register" className="text-blue-400 hover:text-blue-300 hover:underline transition-colors duration-200">
                 Register now
               </Link>
